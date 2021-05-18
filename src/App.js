693d7b8164bc313ac70bb78e9911385e1ea940e2
fatma <fatma.ghaarsallah@gmail.com>
2021-05-18 09:39:33 +0100

    react state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import {Button} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+class state extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: "Fatma Gharsallah",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sagittis venenatis diam, ut placerat sapien malesuada eget. In sodales tortor vitae sapien blandit hendrerit. Nullam molestie risus massa. Curabitur felis sem, bibendum placerat nibh sed, tincidunt pharetra nibh. Duis ut enim porttitor, placerat quam a, accumsan erat. Nam eu augue tortor. Pellentesque mattis felis erat, ac tempus est semper a. Sed tempor ligula nunc, id feugiat dolor ornare sit amet. Suspendisse vitae nisl accumsan, finibus nisl et, vestibulum ipsum. Vestibulum ut congue sem, at suscipit mi.",
+      profession: "Full Stack web developer student",
+      isShown: false,
+      imgSrc:'https://i1.sndcdn.com/avatars-IWAQUb2WuP9Z5hAv-03A2tA-t500x500.jpg'
+  };
+  this.hideProfile = this.hideProfile.bind(this);
+  }
+  hideProfile() {
+    this.setState(state => ({
+      isShown: !state.isShown
+    }));
+}
+render (){
+  const {fullName, bio, profession, isShown, imgSrc} = this.state;
+  return(
+  <div className="App">
+    <Button variant='dark' onClick={() => this.hideProfile()}>{this.state.isShown ? 'Hide it' : 'Show it'}</Button>
+    {isShown && <div className='profile'>
+            <h1 className='title'>
+                {fullName}
+            </h1>
+            <img className='image' src={imgSrc} width='400px' height='400px' alt='avatar' style={{padding:'1% 0'}}/>
+            <p className="bio">
+                {bio}
+            </p>
+            <p className='profession'>
+                {profession}
+            </p>
+    </div>}
+
+  </div>
   );
 }
+}
 
-export default App;
+export default state;
